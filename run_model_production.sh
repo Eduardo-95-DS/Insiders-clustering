87d0124aa45090ee9b03b472bf50b7da81e36d00
Soturno95 <edu.yori@gmail.com>
2023-10-25 11:09:06 -0300

    fixed notebook name

--- run_model_production.sh
+++ run_model_production.sh
@@ -5,4 +5,4 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path='/home/ec2-user/Insiders-clustering'
 path_to_envs='/home/ec2-user/.pyenv/shims/'
 
-$path_to_envs/papermill $path/model/look_10-ear-deploy.ipynb $path/reports/look_10-ear-deploy_$data.ipynb
+$path_to_envs/papermill $path/model/look_10-ear-production-deploy.ipynb $path/reports/look_10-ear-production-deploy_$data.ipynb
